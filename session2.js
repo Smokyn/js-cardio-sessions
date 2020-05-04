61b62f6f6e65870225f97373c100faf4c41a2976
Smokyn <smokyn_cosmin@yahoo.com>
2020-05-04 14:44:11 +0300

    setting up session 2

--- session2.js
+++ session2.js
@@ -1,70 +1,25 @@
-// CHALLENGE 1: ADD ALL NUMBERS
-// Return a sum of all parameters entered regardless of the amount of numbers - NO ARRAYS
-// ex. addAll(2,5,6,7) === 20
+/*1. Scrie o functie care, primeste ca argument o lista si
+ verifica care din elementele listei
+este divizibil cu 2 si intoarce acele elemente.*/
 
-function addAll(...numbers) {
-  /////// ES5 SOLUTION
-  //   var args = Array.prototype.slice.call(arguments);
-  //   var total = 0;
-  //   for (i = 0; i < args.length; i++) {
-  //     total += args[i];
-  //   }
-  //   return total;
-  /////// ES6 SOLUTION FOREACH
-  //   return numbers.reduce((acc, cur) => acc + cur);
-}
+/*2. Scrie o functie care, primeste ca argument o lista si
+ verifica care din elementele listei
+este numar prim si intoarce numerele prime din lista. */
 
-// CHALLENGE 2: SUM ALL PRIMES
-// Pass in a number to loop up to and add all of the prime numbers. A prime number is a whole number greater than 1 whose only factors are 1 and itself
-// ex. sumAllPrimes(10) == 17
+/*3. Scrie o functie care, primeste ca argumente
+ o lista si un numar, apoi verifica daca numarul
+este in lista si intoarce adevarat sau fals. */
 
-function sumAllPrimes(num) {
-  //   let total = 0;
-  //   function checkForPrime(i) {
-  //     for (let j = 2; j < i; j++) {
-  //       if (i % j === 0) {
-  //         return false;
-  //       }
-  //     }
-  //     return true;
-  //   }
-  //   for (let i = 2; i <= num; i++) {
-  //     if (checkForPrime(i)) {
-  //       total += i;
-  //     }
-  //   }
-  //   return total;
-}
-// CHALLENGE 3: SEEK & DESTROY
-// Remove from the array whatever is in the following arguments. Return the leftover numbers in an array
-// ex. seekAndDestroy([2, 3, 4, 6, 6, 'hello'], 2, 6) == [3, 4, 'hello']
+/*4. Scrie o functie care, primeste ca argumente o lista si un numar,
+ apoi verifica de cate ori se afla numarul
+  in lista si intoarce acel rezultat*/
 
-function seekAndDestroy() {}
+/*
+5. Scrie o functie care, primeste ca argumente 2 liste a si b,
+ unde verifica daca elementele
+ din lista a se afla in lista b si intoarce acele elemente. */
 
-// CHALLENGE 4: SORT BY HEIGHT
-// Some people are standing in a row in a park. There are trees between them which cannot be moved. Your task is to rearrange the people by their heights in a non-descending order without moving the trees.
-// ex.
-// a = [-1, 150, 190, 170, -1, -1, 160, 180]
-// sortByHeight(a) == [-1, 150, 160, 170, -1, -1, 180, 190]
-
-function sortByHeight() {}
-
-// CHALLENGE 5: MISSING LETTERS
-// Find the missing letter in the passed letter range and return it. If all letters are present, return undefined
-// ex.
-// missingLetters("abce") == "d"
-// missingLetters("abcdefghjklmno") == "i"
-// missingLetters("abcdefghijklmnopqrstuvwxyz") == undefined
-
-function missingLetters() {}
-
-// CHALLENGE 6: EVEN & ODD SUMS
-// Take in an array and return an array of the sums of even and odd numbers
-// ex.
-// evenOddSums([50, 60, 60, 45, 71]) == [170, 116]
-
-function evenOddSums() {}
-
-const output = sumAllPrimes(10);
-
-console.log(output);
+/*6. Scrie o functie care
+, primeste ca argumente 2 liste a si b,
+ unde verifica daca elementele din lista
+  a se afla in lista b si intoarce acele elemente cat si contorul lor. */
